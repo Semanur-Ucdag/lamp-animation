fd547b28d58ba304787a09ac9874ecf4e835ece2
Semanur-Ucdag <103859993+Semanur-Ucdag@users.noreply.github.com>
2023-03-28 18:32:15 +0300

    openhable lamp animation

---
+++ Lamp/script.js
@@ -0,0 +1,31 @@
+/*const lamp = document.querySelector('.lamp');
+const light = document.querySelector('.lamp-light');
+
+lamp.addEventListener('click', () => {
+    if (light.classList.contains('on')) {
+        light.classList.remove('on');
+        light.classList.add('off');
+    }
+    else {
+        light.classList.remove('off');
+        light.classList.add('on');
+    }
+
+
+})*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const lamp = document.querySelector('.lamp');
+    const light = document.querySelector('.lamp-light');
+  
+    lamp.addEventListener('click', () => {
+        if (light.classList.contains('on')) {
+            light.classList.remove('on');
+            light.classList.add('off');
+        }
+        else {
+            light.classList.remove('off');
+            light.classList.add('on');
+        }
+    });
+  });
